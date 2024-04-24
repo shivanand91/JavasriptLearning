d22e733f03abcc03e42bc65a2e7ad2d1150a21cc
Shivanand Kumar <132545822+shivanand91@users.noreply.github.com>
2024-04-24 17:54:29 +0530

    some editing

---
+++ 01_lecture/script.js
@@ -0,0 +1,8 @@
+
+
+document.getElementById('submit').onclick = function (){
+    numOne = document.getElementById('numOne').value;
+    numTwo = document.getElementById('numTwo').value;
+    result = document.getElementById('result');
+    result = numOne + numTwo;
+}
