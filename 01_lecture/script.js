c77f2cedd9a480c63934f4724c80a79ec7e8fc05
Shivanand Kumar <132545822+shivanand91@users.noreply.github.com>
2024-04-24 18:13:42 +0530

    lecture one

--- 01_lecture/script.js
+++
@@ -1,8 +0,0 @@
-
-
-document.getElementById('submit').onclick = function (){
-    numOne = document.getElementById('numOne').value;
-    numTwo = document.getElementById('numTwo').value;
-    result = document.getElementById('result');
-    result = numOne + numTwo;
-}
---
+++ 01_variable/script.js
@@ -0,0 +1,7 @@
+let fullName = "Shivanand Kumar"
+let age = 22
+let isStudent = true
+
+document.getElementById("p1").textContent = `Your name is ${fullName}`
+document.getElementById("p2").textContent = `Your are ${age} years old.`
+document.getElementById("p3").textContent = `${fullName} is online:- ${isStudent}.`
