42e4bd305648a3610d0a306aef4b5c922da5b617
Shivanand Kumar <132545822+shivanand91@users.noreply.github.com>
2024-04-26 00:25:28 +0530

    Math Object

---
+++ 06_Math object/script.js
@@ -0,0 +1,13 @@
+let x = 24.1985349;
+let y = 55;
+let z;
+
+// z = Math.floor(x) this is use to floor the large floating point number
+// z = Math.random(x) this is use to generate randon number below the given number
+// z = Math.max(4, 8); this is use to find the max value of two number
+// z = Math.round(x); this is use to round the floating point number
+// z = Math.ceil(x); this is use to increase any number by one
+// z = Math.pow(x, y); this is use to find the pow of any number
+z = Math.trunc(x);
+
+console.log(z);
