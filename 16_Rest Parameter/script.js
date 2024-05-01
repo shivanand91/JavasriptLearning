5dcaf795bae03929feac1980d4e49796b03da31c
Shivanand Kumar <132545822+shivanand91@users.noreply.github.com>
2024-05-01 22:03:06 +0530

    UPDATE

--- 16_Rest Parameter/script.js
+++ 16_Rest Parameter/script.js
@@ -5,20 +5,36 @@
                 //   spread = expands an array into seperate elements 
                 //   rest = bundles seperate elements int an array
 
-function openFridge(...foods){
-    console.log(...foods);
+// function openFridge(...foods){
+//     console.log(...foods);
+// }
+
+// function getFood(...foods){
+//     return foods;
+// }
+
+// const food1 = "pizza";
+// const food2 = "burger";
+// const food3 = "hotdog";
+// const food4 = "sushi";
+
+// console.log(food1, food2, food3, food4);
+
+// const foods = getFood(food1, food2, food3, food4);
+// console.log(foods);
+
+
+function sum(...numbers){
+    let result = 0;
+
+    for (let number of numbers) {
+        result += number;        
+    }
+
+    return result;
 }
 
-function getFood(...foods){
-    return foods;
-}
 
-const food1 = "pizza";
-const food2 = "burger";
-const food3 = "hotdog";
-const food4 = "sushi";
+total = sum(1, 2, 3, 4, 5);
 
-console.log(food1, food2, food3, food4);
-
-const foods = getFood(food1, food2, food3, food4);
-console.log(foods);
+console.log(`total sum of array is ${total}`);
