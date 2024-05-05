93f43c84271970a482a805f91db7b32cc818ef04
Shivanand Kumar <132545822+shivanand91@users.noreply.github.com>
2024-05-05 23:02:22 +0530

    type conversion

--- 03_type conversion/TypeConversion.js
+++ 03_type conversion/TypeConversion.js
@@ -11,11 +11,19 @@ let c = true
 // z = Boolean(z);
 
 
-console.log(typeof a);
-console.log(typeof b);
-console.log(typeof c);
+// console.log(typeof a);
+// console.log(typeof b);
+// console.log(typeof c);
 
 
-const inputValue = document.getElementById("inputValue");
-const myResult = document.getElementById("mySubmit");
-const myResult = document.getElementById("inputValue");
+const inputValue = document.getElementById("inputValue").value;
+newValue = Number(inputValue);
+console.log(newValue);
+const mySubmit = document.getElementById("mySubmit");
+const myResult = document.getElementById("myResult");
+
+function check(){
+        myResult.textContent = `input value is ${typeof inputValue}`
+        myResult.textContent = `input value is ${typeof newValue}`
+      
+}
