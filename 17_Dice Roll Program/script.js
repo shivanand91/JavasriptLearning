692cb609a906cf1e9f3a0c682c34ecc418e964d9
Shivanand Kumar <132545822+shivanand91@users.noreply.github.com>
2024-05-10 21:41:57 +0530

    dice roller program

--- 17_Dice Roll Program/script.js
+++ 17_Dice Roll Program/script.js
@@ -10,11 +10,12 @@ function rollDice(){
 
     for(let i= 0; i < numOfDice; i++){
         const value = Math.floor(Math.random() * 6) + 1;
+        console.log(value);
         values.push(value);
         images.push(`<img src="dice_images/${value}.png">`);
     }
 
-    // diceResult.textContent = `dice: ${values.join(', ')}`;
+    diceResult.textContent = `dice: ${values.join(', ')}`;
     diceImage.innerHTML = images.join('');
 
 }
