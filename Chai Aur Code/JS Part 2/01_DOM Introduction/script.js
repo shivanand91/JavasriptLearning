f297592f035a110c8d15b1a863a3577c1f6d3862
Shivanand Kumar <132545822+shivanand91@users.noreply.github.com>
2024-06-04 19:26:33 +0530

    normal code

---
+++ Chai Aur Code/JS Part 2/01_DOM Introduction/script.js
@@ -0,0 +1,6 @@
+
+
+console.log(window);
+console.log(window.document);
+console.log(document);   // this is use to show the code all the doucment
+console.dir(document)   // this line of code is use to print all the hidden info
