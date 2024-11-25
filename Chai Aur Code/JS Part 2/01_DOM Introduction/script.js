c7297164a021c5933a5265cebfb1cf6607807ca8
Shivanand Kumar <132545822+shivanand91@users.noreply.github.com>
2024-11-25 21:27:48 +0530

    Delete Chai Aur Code/JS Part 2/01_DOM Introduction directory

--- Chai Aur Code/JS Part 2/01_DOM Introduction/script.js
+++
@@ -1,6 +0,0 @@
-
-
-console.log(window);
-console.log(window.document);
-console.log(document);   // this is use to show the code all the doucment
-console.dir(document)   // this line of code is use to print all the hidden info
